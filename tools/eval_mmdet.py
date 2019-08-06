d32a42f9e71d6eecb0b561af37d886805de37a0f
LucasX <249048056@qq.com>
2019-08-06 11:44:58 +0800

    Add files via upload

---
+++ tools/eval_mmdet.py
@@ -0,0 +1,50 @@
+import os
+
+import mmdet
+import mmcv
+from mmcv.runner import load_checkpoint
+from mmdet.models import build_detector
+from mmdet.apis import inference_detector, show_result, init_detector
+
+
+def eval_mmdet(config_py='configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py', pretrained_model='work_dirs/faster_rcnn_r50_fpn_1x_voc0712/epoch_100.pth', 
+    img_dir='data/VOCdevkit/VOC2007/JPEGImages'):
+    if mmdet.__version__.startswith('0.6.0'):
+        config_file = config_py
+        checkpoint_file = pretrained_model
+        # build the model from a config file and a checkpoint file
+        model = init_detector(config_file, checkpoint_file, device='cuda:0')
+
+        # test a list of images
+        with open('data/VOCdevkit/VOC2007/ImageSets/Main/test.txt', mode='rt', encoding='utf-8') as f:
+            img_ids = f.readlines()
+        imgs = [os.path.join(img_dir, "{}.jpg".format(img_id.replace('\n', ''))) for img_id in img_ids]
+        print(imgs)
+        for i, result in enumerate(inference_detector(model, imgs)):
+            if not os.path.exists('./eval_mmdet_imgs'):
+                os.makedirs('./eval_mmdet_imgs')
+            
+            print(i, imgs[i])    
+            show_result(imgs[i], result, model.CLASSES, out_file=os.path.join('./eval_mmdet_imgs', imgs[i].split('/')[-1]))
+    else:
+        cfg = mmcv.Config.fromfile(config_py)
+        cfg.model.pretrained = None
+
+        # construct the model and load checkpoint
+        model = build_detector(cfg.model, test_cfg=cfg.test_cfg)
+        _ = load_checkpoint(model, pretrained_model)
+
+        # test a list of images
+        with open('data/VOCdevkit/VOC2007/ImageSets/Main/test.txt', mode='rt', encoding='utf-8') as f:
+            img_ids = f.readlines()
+        imgs = [os.path.join(img_dir, "{}.jpg".format(img_id.replace('\n', ''))) for img_id in img_ids]
+        for i, result in enumerate(inference_detector(model, imgs, cfg, device='cuda:0')):
+            if not os.path.exists('./eval_mmdet_imgs'):
+                os.makedirs('./eval_mmdet_imgs')
+
+            print(i, imgs[i])
+            show_result(imgs[i], result, dataset='voc', score_thr=0.5, out_file=os.path.join('./eval_mmdet_imgs', imgs[i].split('/')[-1]))
+            # print('write to {0}'.format(os.path.join('./eval_mmdet_imgs', imgs[i].split('/')[-1])))
+
+if __name__ == '__main__':
+    eval_mmdet()
